973c4abf89bcbc98dc5fffc4a008c480b2999d17
Eredost <michael.coutin62@gmail.com>
2020-03-03 20:18:14 +0100

    Fixed unreachable assets on Apache server

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,13 +3,14 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
     /**
-     * @Route("/main",
-     *     name="main")
+     * @Route("/",
+     *     name="home")
      */
     public function index()
     {
