543d4c002c303c110f6903c0ad5787b3de4b9585
Eredost <michael.coutin62@gmail.com>
2020-03-20 15:53:41 +0100

    Fixed when invalid or page value is too high on potential malicious user input

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Repository\QuestionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
@@ -13,14 +14,28 @@ class MainController extends AbstractController
      * @Route("/",
      *     methods={"GET"},
      *     name="home")
+     *
+     * @param QuestionRepository $questionRepository
+     * @param Request            $request
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function home(QuestionRepository $questionRepository, Request $request)
     {
         $currentPage = $request->query->get('page', 1);
+        if (!preg_match('/\d+/', $currentPage)
+            || $currentPage < 1) {
+            $currentPage = 1;
+        }
 
         $questions = $questionRepository->findAllQuestionsWithTags($currentPage, 10);
         $questions->lastPage = intval(ceil($questions->count() / 10));
         $questions->currentPage = intval($currentPage);
+
+        if ($currentPage > $questions->lastPage) {
+
+            throw new NotFoundHttpException('The page you are looking for, didn\'t exist');
+        }
 
         return $this->render('frontend/main/index.html.twig', [
             'questions' => $questions,
