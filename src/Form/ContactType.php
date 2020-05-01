8a79cc4e24493910cb4f2ad676ce6e39f40c3039
Eredost <michael.coutin62@gmail.com>
2020-05-01 15:02:10 +0200

    feat: add validators for contact form fields

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -9,6 +9,9 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class ContactType extends AbstractType
 {
@@ -16,25 +19,60 @@ class ContactType extends AbstractType
     {
         $builder
             ->add('firstname', TextType::class, [
-                'required' => false,
-                'attr'     => [
+                'required'    => false,
+                'attr'        => [
                     'placeholder' => 'John',
+                ],
+                'constraints' => [
+                    new Length([
+                        'min'        => 3,
+                        'max'        => 20,
+                        'minMessage' => 'The firstname should contain at least {{ limit }} characters',
+                        'maxMessage' => 'The firstname should contain a maximum of {{ limit }} characters',
+                    ]),
                 ],
             ])
             ->add('lastname', TextType::class, [
-                'required' => false,
-                'attr'     => [
+                'required'    => false,
+                'attr'        => [
                     'placeholder' => 'Doe',
+                ],
+                'constraints' => [
+                    new Length([
+                        'min'        => 3,
+                        'max'        => 20,
+                        'minMessage' => 'The lastname should contain at least {{ limit }} characters',
+                        'maxMessage' => 'The lastname should contain a maximum of {{ limit }} characters',
+                    ]),
                 ],
             ])
             ->add('email', EmailType::class, [
-                'attr' => [
+                'attr'        => [
                     'placeholder' => 'john.doe@email.com',
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'The email could not be blank',
+                    ]),
+                    new Email([
+                        'message' => 'The email is not valid',
+                    ]),
                 ],
             ])
             ->add('message', TextareaType::class, [
-                'attr' => [
+                'attr'        => [
                     'placeholder' => 'Your comment here...',
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'The message could not be blank',
+                    ]),
+                    new Length([
+                        'min'        => 10,
+                        'max'        => 2500,
+                        'minMessage' => 'The message should contain at least {{ limit }} characters',
+                        'maxMessage' => 'The message should contain a maximum of {{ limit }} characters',
+                    ])
                 ],
             ])
         ;
