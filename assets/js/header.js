af6cf8fcb19ae6d08508e9df744ddccd3bda053b
Eredost <michael.coutin62@gmail.com>
2020-05-02 18:41:05 +0200

    feat: add profile menu in the header, allow you to go to the account details and be able to log out

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,17 +1,25 @@
 const searchFormClassName = 'search__form';
 const searchIconClassName = 'search__icon';
-const searchInputClassName = 'q';
+const searchInputName = 'q';
+const profileClassName = 'profile__registered';
+const profileMenuClassName = 'profile__menu';
 
 export default class Header {
     constructor(headerClassName) {
         this.header = document.querySelector(`.${headerClassName}`);
         this.searchFormElement = document.querySelector(`.${searchIconClassName} + .${searchFormClassName}`);
-        this.searchInputElement = document.querySelector(`.${this.header.className} input[name="${searchInputClassName}"]`);
+        this.searchInputElement = document.querySelector(`.${this.header.className} input[name="${searchInputName}"]`);
+
+        this.profileElement = document.querySelector(`.${this.header.className} .${profileClassName}`);
+        this.profileMenuElement = this.profileElement.querySelector(`.${profileMenuClassName}`);
 
         let searchIconElement = document.querySelector(`.${this.header.className} .${searchIconClassName}`);
 
         searchIconElement.addEventListener('click', this.handleSearchIconClick.bind(this));
         this.searchInputElement.addEventListener('blur', this.handleSearchInputBlur.bind(this));
+
+        this.profileElement.addEventListener('mouseover', this.handleProfileMouseOver.bind(this));
+        this.profileElement.addEventListener('mouseout', this.handleProfileMouseOut.bind(this));
     }
 
     handleSearchInputBlur() {
@@ -21,5 +29,13 @@ export default class Header {
     handleSearchIconClick(event) {
         this.searchFormElement.classList.remove('hidden');
         this.searchInputElement.focus();
+    }
+
+    handleProfileMouseOver() {
+        this.profileMenuElement.classList.remove('hidden');
+    }
+
+    handleProfileMouseOut() {
+        this.profileMenuElement.classList.add('hidden');
     }
 }
