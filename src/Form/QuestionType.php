6ef30cc2ed0fd99409efb70204e0c34f79249685
Eredost <michael.coutin62@gmail.com>
2020-05-27 10:54:53 +0200

    feat: implementation of the functionality to edit a question

--- src/Form/QuestionType.php
+++ src/Form/QuestionType.php
@@ -9,21 +9,10 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormEvent;
-use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Security\Core\Security;
 
 class QuestionType extends AbstractType
 {
-    /** @var Security $security */
-    private $security;
-
-    public function __construct(Security $security)
-    {
-        $this->security = $security;
-    }
-
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
@@ -35,15 +24,7 @@ class QuestionType extends AbstractType
                 'expanded' => false,
                 'multiple' => true,
             ])
-            ->addEventListener(FormEvents::POST_SUBMIT, [$this, 'onPreSubmit'])
         ;
-    }
-
-    public function onPreSubmit(FormEvent $event)
-    {
-        /** @var Question $question */
-        $question = $event->getData();
-        $question->setUser($this->security->getUser());
     }
 
     public function configureOptions(OptionsResolver $resolver)
